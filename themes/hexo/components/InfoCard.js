dabb6389bb48e9677effea98b086cb0cc70ff297
ingmeing <90748341+ingmeing@users.noreply.github.com>
2023-10-24 13:13:57 +0800

    Update InfoCard.js

--- themes/hexo/components/InfoCard.js
+++ themes/hexo/components/InfoCard.js
@@ -21,8 +21,8 @@ export function InfoCard(props) {
                   router.push('/')
                 }}
             >
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <LazyImage src={siteInfo?.icon} className='rounded-full' width={120} alt={BLOG.AUTHOR} />
+                {/* eslint-disable-next-line @next/next/no-img-element className='rounded-full' */}
+                <LazyImage src={siteInfo?.icon} width={120} alt={BLOG.AUTHOR} />
             </div>
             <div className='font-medium text-center text-xl pb-4'>{BLOG.AUTHOR}</div>
             <div className='text-sm text-center'>{BLOG.BIO}</div>
