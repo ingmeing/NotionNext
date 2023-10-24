3e5866d6fad5f04bef100a1ca4995286f41decd0
ingmeing <90748341+ingmeing@users.noreply.github.com>
2023-10-24 10:13:02 +0800

    Update Hero.js

--- themes/hexo/components/Hero.js
+++ themes/hexo/components/Hero.js
@@ -57,7 +57,7 @@ const Hero = props => {
 
             <div className="text-white absolute bottom-0 flex flex-col h-full items-center justify-center w-full ">
                 {/* 站点标题 */}
-                <div className='font-black text-4xl md:text-5xl shadow-text'>Be Your Lifepartner🤗</div> 
+                <div className='font-black text-4xl md:text-5xl shadow-text'>小小改变 就趁现在</div> 
                 //{siteInfo?.title}
                 {/* 站点欢迎语 */}
                 <div className='mt-2 h-12 items-center text-center font-medium shadow-text text-lg'>
